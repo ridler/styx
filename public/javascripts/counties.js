d1fa6b8a314207176c9ea7846585bf6348c2bf67
ridler <forrest.ridler@colorado.edu>
2015-04-28 18:47:25 -0600

    restyled counties

--- public/javascripts/counties.js
+++ public/javascripts/counties.js
@@ -2,23 +2,31 @@
 
   var app = angular.module('countyUI', []);
 
+  app.directive('loadingCircle', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/loading-circle.html'
+    }
+  });
+
   app.controller('graphics', ['$http', '$scope', function($http, $scope) {
     var ui = this;
     ui.loaded = false;
-    ui.geodata = null;
 
     var map = L.map('county-map').setView([38.50, -95.35], 4);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    var count = 0; var colors = ['red', 'yellow', 'green', 'blue'];
     $http.get('/geoCounties.json').success(function(data) {
-      console.log(data.features[0]);
       L.geoJson(data, {
         style: function(feature) {
+          var i = Math.ceil(9*Math.random());
+          var c = '#FF'+i+''+i+''+i+'0';
           return {
-            color: '#F8F'+Math.ceil(10*Math.random()+'0'),
-            weight: 1
+            color: 'red',
+            weight: 0.2
           };
         },
         onEachFeature: function(feature, layer) {
